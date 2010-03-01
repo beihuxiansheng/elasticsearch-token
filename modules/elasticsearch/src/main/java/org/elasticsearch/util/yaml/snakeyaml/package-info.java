4a46a7e498ef1c02033a0f8f43d37187a171ac35
kimchy <kimchy@gmail.com>
2010-03-01 15:56:19 +0200

    merge snakeyaml into source
    Former-commit-id: 234a5abda0ee2c315a944e81007f439868bb2384

--- modules/elasticsearch/src/main/java/org/elasticsearch/util/yaml/snakeyaml/package-info.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/util/yaml/snakeyaml/package-info.java
@@ -3,9 +3,13 @@ begin_comment
 comment|/*  * Licensed to Elastic Search and Shay Banon under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership. Elastic Search licenses this  * file to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *    http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied.  See the License for the  * specific language governing permissions and limitations  * under the License.  */
 end_comment
 
-begin_decl_stmt
-DECL|external variable declarations|org.elasticsearch.util.yaml.snakeyaml
-name|ackage
+begin_comment
+comment|/**  * Stripped down snakeyaml (1.6). By Andrey Somov, Apache 2.  */
+end_comment
+
+begin_package
+DECL|package|org.elasticsearch.util.yaml.snakeyaml
+package|package
 name|org
 operator|.
 name|elasticsearch
@@ -15,8 +19,8 @@ operator|.
 name|yaml
 operator|.
 name|snakeyaml
-decl_stmt|;
-end_decl_stmt
+package|;
+end_package
 
 end_unit
 
