237bebab8cdfb090a2887322491a2c48c9726dd0
kimchy <kimchy@gmail.com>
2010-05-24 23:27:07 +0300

    add async http client
    Former-commit-id: 32e4c405de255151f71420876ae9d1a0deb41e51

---
+++ modules/elasticsearch/src/main/java/org/elasticsearch/util/http/package-info.java
@@ -0,0 +1,20 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Ning Async-Client, revision 1fbadf5, date: 2010-05-24.  *  * Changes:  *  * 1. Use elasticsearch google collect.  * 2. Change to use ESLogger instead of log4j.  * 3. AsyncHttpClient: * Change string based provider name  * 4. AsyncHttpClientConfig: Change userAgent to ES   */
+end_comment
+
+begin_package
+DECL|package|org.elasticsearch.util.http
+package|package
+name|org
+operator|.
+name|elasticsearch
+operator|.
+name|util
+operator|.
+name|http
+package|;
+end_package
+
+end_unit
+
