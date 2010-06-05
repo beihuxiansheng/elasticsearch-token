2af991e9c9f13e4406a130e499f1520b3330c7d1
kimchy <kimchy@gmail.com>
2010-06-05 21:59:42 +0300

    update doc
    Former-commit-id: 57a21bedd2ed4cfe8a863bfbe477dfae9ba6676e

--- modules/elasticsearch/src/main/java/org/elasticsearch/search/facets/collector/FacetCollector.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/search/facets/collector/FacetCollector.java
@@ -47,7 +47,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * @author kimchy (Shay Banon)  */
+comment|/**  * @author kimchy (shay.banon)  */
 end_comment
 
 begin_class
--- modules/elasticsearch/src/main/java/org/elasticsearch/search/facets/query/InternalQueryFacet.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/search/facets/query/InternalQueryFacet.java
@@ -105,7 +105,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * @author kimchy (Shay Banon)  */
+comment|/**  * @author kimchy (shay.banon)  */
 end_comment
 
 begin_class
