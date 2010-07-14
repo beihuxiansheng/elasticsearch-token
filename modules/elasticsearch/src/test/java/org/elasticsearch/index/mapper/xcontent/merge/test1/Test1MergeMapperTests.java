3f2b7a4d182770c6f9ede9639fda04f50035782b
kimchy <kimchy@gmail.com>
2010-07-14 07:32:17 +0300

    Make merging mappings smarter, closes #253.
    Former-commit-id: 234455530af05b7a9958a310bda63a8e65b1ef29

--- modules/elasticsearch/src/test/java/org/elasticsearch/index/mapper/xcontent/merge/test1/Test1MergeMapperTests.java
+++ modules/elasticsearch/src/test/java/org/elasticsearch/index/mapper/xcontent/merge/test1/Test1MergeMapperTests.java
@@ -281,7 +281,7 @@ argument_list|()
 argument_list|,
 name|equalTo
 argument_list|(
-literal|true
+literal|false
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -330,7 +330,7 @@ argument_list|()
 argument_list|,
 name|equalTo
 argument_list|(
-literal|true
+literal|false
 argument_list|)
 argument_list|)
 expr_stmt|;
