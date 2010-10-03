66967be7eeae2e58c5b696e3a6977023a3e563ec
kimchy <kimchy@gmail.com>
2010-10-03 22:22:45 +0200

    Wikipedia River: A river to index wikipedia, closes #403.
    Former-commit-id: c4d17860a1e4ee48cb62ae2cd8a04e87f6f3dde4

---
+++ plugins/river/wikipedia/src/main/java/org/elasticsearch/river/wikipedia/support/package-info.java
@@ -0,0 +1,22 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Copied from wikixmlj on 2010-10-03.  *  * Changed from File handling to URL handling, and removed Dom parser.  */
+end_comment
+
+begin_package
+DECL|package|org.elasticsearch.river.wikipedia.support
+package|package
+name|org
+operator|.
+name|elasticsearch
+operator|.
+name|river
+operator|.
+name|wikipedia
+operator|.
+name|support
+package|;
+end_package
+
+end_unit
+
