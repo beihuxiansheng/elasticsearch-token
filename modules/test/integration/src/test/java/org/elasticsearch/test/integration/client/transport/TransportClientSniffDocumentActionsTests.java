632dd3a28df6d4bb3b571cc4aa2bf19acab53147
kimchy <kimchy@gmail.com>
2010-12-17 19:31:28 +0200

    Java TransportClient: By default, don't sniff other nodes and use addresses provided as is, closes #565.
    Former-commit-id: 415bb5d7f3c745f331be60aa810fd1f92fcc16eb

--- modules/test/integration/src/test/java/org/elasticsearch/test/integration/client/transport/TransportClientSniffDocumentActionsTests.java
+++ modules/test/integration/src/test/java/org/elasticsearch/test/integration/client/transport/TransportClientSniffDocumentActionsTests.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  * Licensed to Elastic Search and Shay Banon under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership. Elastic Search licenses this  * file to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied.  See the License for the  * specific language governing permissions and limitations  * under the License.  */
+comment|/*  * Licensed to Elastic Search and Shay Banon under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership. Elastic Search licenses this  * file to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *    http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied.  See the License for the  * specific language governing permissions and limitations  * under the License.  */
 end_comment
 
 begin_package
@@ -123,10 +123,10 @@ comment|/**  * @author kimchy (shay.banon)  */
 end_comment
 
 begin_class
-DECL|class|TransportClientDocumentActionsTests
+DECL|class|TransportClientSniffDocumentActionsTests
 specifier|public
 class|class
-name|TransportClientDocumentActionsTests
+name|TransportClientSniffDocumentActionsTests
 extends|extends
 name|DocumentActionsTests
 block|{
@@ -180,7 +180,7 @@ name|put
 argument_list|(
 literal|"client.transport.sniff"
 argument_list|,
-literal|false
+literal|true
 argument_list|)
 operator|.
 name|build
@@ -248,7 +248,7 @@ name|put
 argument_list|(
 literal|"client.transport.sniff"
 argument_list|,
-literal|false
+literal|true
 argument_list|)
 operator|.
 name|build
