df830d038049b346583f2eae6b11eb65509c353a
Shay Banon <kimchy@gmail.com>
2011-07-29 23:36:02 +0300

    Analysis: 'truncate' token filter, closes #1165.
    Former-commit-id: 06bcd4253bb5b0a32f53b55f1685d16db479e64a

--- modules/elasticsearch/src/main/java/org/elasticsearch/index/analysis/AnalysisModule.java
+++ modules/elasticsearch/src/main/java/org/elasticsearch/index/analysis/AnalysisModule.java
@@ -2135,6 +2135,17 @@ operator|.
 name|class
 argument_list|)
 expr_stmt|;
+name|tokenFiltersBindings
+operator|.
+name|processTokenFilter
+argument_list|(
+literal|"truncate"
+argument_list|,
+name|TruncateTokenFilterFactory
+operator|.
+name|class
+argument_list|)
+expr_stmt|;
 block|}
 DECL|method|processTokenizers
 annotation|@
