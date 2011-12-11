2a84f1a781290d9082649f6c87b8a35df5d3d9e3
Shay Banon <kimchy@gmail.com>
2011-12-11 18:59:12 +0200

    remove author names (we no longer have them on) and fix header
    Former-commit-id: 1901efb64ae6605590cf0ecadd4ee7eb69d6efa5

--- src/main/java/org/elasticsearch/index/analysis/IcuTransformTokenFilterFactory.java
+++ src/main/java/org/elasticsearch/index/analysis/IcuTransformTokenFilterFactory.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  * Licensed to Elastic Search and Shay Banon under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership. Elastic Search licenses this  * file to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *    http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied.  See the License for the  * specific language governing permissions and limitations  * under the License.  */
+comment|/*  * Licensed to ElasticSearch and Shay Banon under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership. ElasticSearch licenses this  * file to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *    http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied.  See the License for the  * specific language governing permissions and limitations  * under the License.  */
 end_comment
 
 begin_package
@@ -15,6 +15,20 @@ operator|.
 name|analysis
 package|;
 end_package
+
+begin_import
+import|import
+name|com
+operator|.
+name|ibm
+operator|.
+name|icu
+operator|.
+name|text
+operator|.
+name|Transliterator
+import|;
+end_import
 
 begin_import
 import|import
@@ -73,20 +87,6 @@ operator|.
 name|assistedinject
 operator|.
 name|Assisted
-import|;
-end_import
-
-begin_import
-import|import
-name|com
-operator|.
-name|ibm
-operator|.
-name|icu
-operator|.
-name|text
-operator|.
-name|Transliterator
 import|;
 end_import
 
@@ -160,9 +160,9 @@ specifier|final
 name|Transliterator
 name|transliterator
 decl_stmt|;
-DECL|method|IcuTransformTokenFilterFactory
 annotation|@
 name|Inject
+DECL|method|IcuTransformTokenFilterFactory
 specifier|public
 name|IcuTransformTokenFilterFactory
 parameter_list|(
@@ -254,9 +254,9 @@ name|dir
 argument_list|)
 expr_stmt|;
 block|}
-DECL|method|create
 annotation|@
 name|Override
+DECL|method|create
 specifier|public
 name|TokenStream
 name|create
--- src/test/java/org/elasticsearch/index/analysis/SimpleIcuAnalysisTests.java
+++ src/test/java/org/elasticsearch/index/analysis/SimpleIcuAnalysisTests.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  * Licensed to Elastic Search and Shay Banon under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership. Elastic Search licenses this  * file to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *    http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied.  See the License for the  * specific language governing permissions and limitations  * under the License.  */
+comment|/*  * Licensed to ElasticSearch and Shay Banon under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership. ElasticSearch licenses this  * file to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *    http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied.  See the License for the  * specific language governing permissions and limitations  * under the License.  */
 end_comment
 
 begin_package
@@ -184,7 +184,7 @@ name|ImmutableSettings
 operator|.
 name|Builder
 operator|.
-name|*
+name|EMPTY_SETTINGS
 import|;
 end_import
 
@@ -196,12 +196,12 @@ name|hamcrest
 operator|.
 name|Matchers
 operator|.
-name|*
+name|instanceOf
 import|;
 end_import
 
 begin_comment
-comment|/**  * @author kimchy (shay.banon)  */
+comment|/**  */
 end_comment
 
 begin_class
@@ -210,9 +210,9 @@ specifier|public
 class|class
 name|SimpleIcuAnalysisTests
 block|{
-DECL|method|testDefaultsIcuAnalysis
 annotation|@
 name|Test
+DECL|method|testDefaultsIcuAnalysis
 specifier|public
 name|void
 name|testDefaultsIcuAnalysis
