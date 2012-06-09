3bada507808a10aa1122126dc17694ee74e2cd15
Shay Banon <kimchy@gmail.com>
2012-06-10 00:42:54 +0200

    Upgrade to latest jst166y and jsr166e                                                                                                                                                                                            Embed the code now in our source, since jsr166e jar generation with 1.6 instead of 1.7 is complicated when doing it on its own as it relies on ThreadLocalRandom, and we have it in jsr166y
    Former-commit-id: 8f0bc799c67a2dbc97aaa291e707e15fc52233b8

---
+++ src/main/java/jsr166e/package-info.java
@@ -0,0 +1,18 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * Written by Doug Lea with assistance from members of JCP JSR-166  * Expert Group and released to the public domain, as explained at  * http://creativecommons.org/publicdomain/zero/1.0/  */
+end_comment
+
+begin_comment
+comment|// Built on 2012-06-09
+end_comment
+
+begin_package
+DECL|package|jsr166e
+package|package
+name|jsr166e
+package|;
+end_package
+
+end_unit
+
