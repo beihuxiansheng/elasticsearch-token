da53dd286efa5de4808dcff01139b1f258e5eb90
Shay Banon <kimchy@gmail.com>
2012-06-12 17:26:36 +0200

    move remaining CHM construction to the factory method
    Former-commit-id: ab35e7ce7a1dda2c8ed49928dc66f6ad99f67bb3

--- src/main/java/org/elasticsearch/common/util/concurrent/ConcurrentCollections.java
+++ src/main/java/org/elasticsearch/common/util/concurrent/ConcurrentCollections.java
@@ -20,6 +20,14 @@ end_package
 
 begin_import
 import|import
+name|jsr166e
+operator|.
+name|ConcurrentHashMapV8
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|elasticsearch
