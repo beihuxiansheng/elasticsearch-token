755405634da240fbb016f71fc9863cafef91b8df
David Pilato <david@pilato.fr>
2013-01-10 17:27:35 +0100

    Support for REST get ALL templates. /_template shows: No handler found for uri [/_template] and method [GET]
    It would make sense to list the templates as they are listed in the /_cluster/state call.
    
    Closes #2532.
    
    
    Former-commit-id: ea4988e9dcbac19c19156bb81dc171bd88a89595

--- src/main/java/org/elasticsearch/client/IndicesAdminClient.java
+++ src/main/java/org/elasticsearch/client/IndicesAdminClient.java
@@ -2750,11 +2750,23 @@ name|listener
 parameter_list|)
 function_decl|;
 comment|/**      * Gets an index template.      *      * @param name The name of the template.      */
+annotation|@
+name|Deprecated
 DECL|method|prepareGetTemplates
 name|GetIndexTemplatesRequestBuilder
 name|prepareGetTemplates
 parameter_list|(
 name|String
+name|name
+parameter_list|)
+function_decl|;
+comment|/**      * Gets an index template (optional).      */
+DECL|method|prepareGetTemplates
+name|GetIndexTemplatesRequestBuilder
+name|prepareGetTemplates
+parameter_list|(
+name|String
+modifier|...
 name|name
 parameter_list|)
 function_decl|;
