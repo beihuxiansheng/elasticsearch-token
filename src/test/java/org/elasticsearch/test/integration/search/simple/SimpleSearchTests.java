4cec51ed60b704e0a0396ef2c66218c2e759037b
Simon Willnauer <simonw@apache.org>
2013-01-18 20:30:31 +0100

    Respect lowercase_expanded_terms in MappingQueryParser
    Fixes #2566
    
    
    Former-commit-id: d6b613ac8c6e3ddee6d8e1604db208721f03cb75

--- src/test/java/org/elasticsearch/test/integration/search/simple/SimpleSearchTests.java
+++ src/test/java/org/elasticsearch/test/integration/search/simple/SimpleSearchTests.java
@@ -726,6 +726,11 @@ name|queryString
 argument_list|(
 literal|"_id:XXX*"
 argument_list|)
+operator|.
+name|lowercaseExpandedTerms
+argument_list|(
+literal|false
+argument_list|)
 argument_list|)
 operator|.
 name|execute
