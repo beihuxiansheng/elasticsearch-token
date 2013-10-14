efe0f338c8761200b3f094b2619e4d7115dad714
Simon Willnauer <simonw@apache.org>
2013-10-14 14:48:41 +0200

    Disable testSegmentsWithMergeFlag with AwaitsFix
    Former-commit-id: 08db1fb292f30c3927c287f1429707bc0389ddd4

--- src/test/java/org/elasticsearch/index/engine/robin/RobinEngineTests.java
+++ src/test/java/org/elasticsearch/index/engine/robin/RobinEngineTests.java
@@ -134,6 +134,22 @@ begin_import
 import|import
 name|org
 operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|util
+operator|.
+name|LuceneTestCase
+operator|.
+name|AwaitsFix
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
 name|elasticsearch
 operator|.
 name|ExceptionsHelper
@@ -3000,6 +3016,13 @@ expr_stmt|;
 block|}
 annotation|@
 name|Test
+annotation|@
+name|AwaitsFix
+argument_list|(
+name|bugUrl
+operator|=
+literal|"kimchy is taking care of the fix"
+argument_list|)
 DECL|method|testSegmentsWithMergeFlag
 specifier|public
 name|void
