380be07228722b7aca16f8d8d0844eaf2ce3a8e1
Igor Motov <igor@motovs.org>
2013-11-03 21:20:38 -0500

    Initial implementation of ResourceWatcherService
    Closes #4062
    
    
    Former-commit-id: c724f0de5dc8108acbbef5034249c1b99388a113

---
+++ src/main/java/org/elasticsearch/watcher/FileChangesListener.java
@@ -0,0 +1,111 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * Licensed to ElasticSearch and Shay Banon under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership. ElasticSearch licenses this  * file to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *    http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied.  See the License for the  * specific language governing permissions and limitations  * under the License.  */
+end_comment
+
+begin_package
+DECL|package|org.elasticsearch.watcher
+package|package
+name|org
+operator|.
+name|elasticsearch
+operator|.
+name|watcher
+package|;
+end_package
+
+begin_import
+import|import
+name|java
+operator|.
+name|io
+operator|.
+name|File
+import|;
+end_import
+
+begin_comment
+comment|/**  * Callback interface that file changes File Watcher is using to notify listeners about changes.  */
+end_comment
+
+begin_class
+DECL|class|FileChangesListener
+specifier|public
+class|class
+name|FileChangesListener
+block|{
+comment|/**      * Called for every file found in the watched directory during initialization      */
+DECL|method|onFileInit
+specifier|public
+name|void
+name|onFileInit
+parameter_list|(
+name|File
+name|file
+parameter_list|)
+block|{      }
+comment|/**      * Called for every subdirectory found in the watched directory during initialization      */
+DECL|method|onDirectoryInit
+specifier|public
+name|void
+name|onDirectoryInit
+parameter_list|(
+name|File
+name|file
+parameter_list|)
+block|{      }
+comment|/**      * Called for every new file found in the watched directory      */
+DECL|method|onFileCreated
+specifier|public
+name|void
+name|onFileCreated
+parameter_list|(
+name|File
+name|file
+parameter_list|)
+block|{      }
+comment|/**      * Called for every file that disappeared in the watched directory      */
+DECL|method|onFileDeleted
+specifier|public
+name|void
+name|onFileDeleted
+parameter_list|(
+name|File
+name|file
+parameter_list|)
+block|{      }
+comment|/**      * Called for every file that was changed in the watched directory      */
+DECL|method|onFileChanged
+specifier|public
+name|void
+name|onFileChanged
+parameter_list|(
+name|File
+name|file
+parameter_list|)
+block|{      }
+comment|/**      * Called for every new subdirectory found in the watched directory      */
+DECL|method|onDirectoryCreated
+specifier|public
+name|void
+name|onDirectoryCreated
+parameter_list|(
+name|File
+name|file
+parameter_list|)
+block|{      }
+comment|/**      * Called for every file that disappeared in the watched directory      */
+DECL|method|onDirectoryDeleted
+specifier|public
+name|void
+name|onDirectoryDeleted
+parameter_list|(
+name|File
+name|file
+parameter_list|)
+block|{      }
+block|}
+end_class
+
+end_unit
+
