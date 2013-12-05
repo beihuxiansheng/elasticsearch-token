370054a708ebd4b77ec71e92f7f9a2c676b37e64
Shay Banon <kimchy@gmail.com>
2013-12-05 14:36:27 +0100

    remove help command, list all cat commands in /_cat?h endpoint
    Former-commit-id: 28eff2ba2942312a4bb2664d25357161ba105e43

--- src/main/java/org/elasticsearch/rest/action/RestActionModule.java
+++ src/main/java/org/elasticsearch/rest/action/RestActionModule.java
@@ -2617,16 +2617,6 @@ operator|.
 name|asEagerSingleton
 argument_list|()
 expr_stmt|;
-name|bind
-argument_list|(
-name|RestHelpAction
-operator|.
-name|class
-argument_list|)
-operator|.
-name|asEagerSingleton
-argument_list|()
-expr_stmt|;
 block|}
 block|}
 end_class
