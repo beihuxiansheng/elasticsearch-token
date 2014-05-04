fe99c936a5052a55686b4b959e3258a812bc03a4
Adrien Grand <jpountz@gmail.com>
2014-05-04 11:58:30 +0200

    [TEST] Disable REST tests until #6033 is fixed.
    Former-commit-id: 01eb01cb70dca97fd9b6faafe9a4dece3a9f164c

--- src/test/java/org/elasticsearch/test/rest/ElasticsearchRestTests.java
+++ src/test/java/org/elasticsearch/test/rest/ElasticsearchRestTests.java
@@ -62,6 +62,22 @@ begin_import
 import|import
 name|org
 operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|util
+operator|.
+name|LuceneTestCase
+operator|.
+name|AwaitsFix
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
 name|elasticsearch
 operator|.
 name|Version
@@ -331,6 +347,13 @@ comment|//@ReplicateOnEachVm
 end_comment
 
 begin_class
+annotation|@
+name|AwaitsFix
+argument_list|(
+name|bugUrl
+operator|=
+literal|"https://github.com/elasticsearch/elasticsearch/issues/6033"
+argument_list|)
 DECL|class|ElasticsearchRestTests
 specifier|public
 class|class
