18cd4aecf37735b60b5acd273797c886f8a9b34d
mikemccand <mail@mikemccandless.com>
2014-05-20 10:28:31 -0400

    Don't use AllTokenStream when no fields were boosted
    AllTokenStream, used to index the _all field, adds some overhead, but
    it's not necessary when no fields were boosted or when positions are
    not indexed the _all field.
    
    Closes #6187 Closes #6219
    
    
    Former-commit-id: 9c45fe8f9b37500ccd36d5bdd5052f29cb84b432

--- src/main/java/org/elasticsearch/index/mapper/internal/AllFieldMapper.java
+++ src/main/java/org/elasticsearch/index/mapper/internal/AllFieldMapper.java
@@ -1113,10 +1113,15 @@ name|fieldType
 operator|.
 name|indexOptions
 argument_list|()
-operator|==
+operator|.
+name|compareTo
+argument_list|(
 name|IndexOptions
 operator|.
 name|DOCS_AND_FREQS_AND_POSITIONS
+argument_list|)
+operator|>=
+literal|0
 condition|)
 block|{
 return|return
