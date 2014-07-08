5beb1d6508cd986f11040189245715a1f8d326c5
Boaz Leskes <b.leskes@gmail.com>
2014-07-08 13:34:24 +0200

    Revert "[Discovery] immediately start Master|Node fault detection pinging"
    In #6706 we change the master validation to start pining immediately after a new master as ellected or a node joined. The idea is to have a quicker response to failures. This does however create a problem if the new master has yet fully processed it's ellection and responds to the ping with a NoLongerMasterException. This causes the source node to remove the current master and ellect another, only to find out it's not a master either and so forth. We are moving this change to the feature/improve_zen branch, where the improvements we made will cause the situation to be handled properly.
    
    This reverts commit 701d7dbab2db65c3ed7ad5dc0940e49605d077c1 [formerly ae16956e072bea317ea481f65f2e110dc48fde17].
    
    
    Former-commit-id: caf11ff2fbd9195cc780edb375f6a74302d906df

--- src/main/java/org/elasticsearch/discovery/zen/fd/MasterFaultDetection.java
+++ src/main/java/org/elasticsearch/discovery/zen/fd/MasterFaultDetection.java
@@ -760,17 +760,12 @@ operator|new
 name|MasterPinger
 argument_list|()
 expr_stmt|;
-comment|// we use schedule with a 0 time value to run the pinger on the pool as it will run on later
+comment|// start the ping process
 name|threadPool
 operator|.
 name|schedule
 argument_list|(
-name|TimeValue
-operator|.
-name|timeValueMillis
-argument_list|(
-literal|0
-argument_list|)
+name|pingInterval
 argument_list|,
 name|ThreadPool
 operator|.
@@ -966,17 +961,11 @@ operator|new
 name|MasterPinger
 argument_list|()
 expr_stmt|;
-comment|// we use schedule with a 0 time value to run the pinger on the pool as it will run on later
 name|threadPool
 operator|.
 name|schedule
 argument_list|(
-name|TimeValue
-operator|.
-name|timeValueMillis
-argument_list|(
-literal|0
-argument_list|)
+name|pingInterval
 argument_list|,
 name|ThreadPool
 operator|.
--- src/main/java/org/elasticsearch/discovery/zen/fd/NodesFaultDetection.java
+++ src/main/java/org/elasticsearch/discovery/zen/fd/NodesFaultDetection.java
@@ -651,17 +651,11 @@ name|NodeFD
 argument_list|()
 argument_list|)
 expr_stmt|;
-comment|// we use schedule with a 0 time value to run the pinger on the pool as it will run on later
 name|threadPool
 operator|.
 name|schedule
 argument_list|(
-name|TimeValue
-operator|.
-name|timeValueMillis
-argument_list|(
-literal|0
-argument_list|)
+name|pingInterval
 argument_list|,
 name|ThreadPool
 operator|.
@@ -854,17 +848,11 @@ name|NodeFD
 argument_list|()
 argument_list|)
 expr_stmt|;
-comment|// we use schedule with a 0 time value to run the pinger on the pool as it will run on later
 name|threadPool
 operator|.
 name|schedule
 argument_list|(
-name|TimeValue
-operator|.
-name|timeValueMillis
-argument_list|(
-literal|0
-argument_list|)
+name|pingInterval
 argument_list|,
 name|ThreadPool
 operator|.
