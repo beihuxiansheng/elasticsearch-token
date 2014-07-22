acafa73dcfb1c34b0cc2082b8bd351e60a0af2a5
Brian Murphy <brian.p.murphy@gmail.com>
2014-07-22 13:58:07 +0100

    [TEST] Fix indexed script test.
    Former-commit-id: 0de30e1798505ecc6ee8a2636b5d86474d3677f8

--- src/test/java/org/elasticsearch/script/IndexedScriptTests.java
+++ src/test/java/org/elasticsearch/script/IndexedScriptTests.java
@@ -357,7 +357,7 @@ decl_stmt|;
 name|String
 name|query
 init|=
-literal|"{ \"query\" : { \"match_all\": {}} , \"script_fields\" : { \"test1\" : { \"id\" : \"script1\", \"lang\":\"groovy\" }, \"test2\" : { \"id\" : \"script2\", \"lang\":\"groovy\", \"params\":{\"factor\":3}  }}, size:1}"
+literal|"{ \"query\" : { \"match_all\": {}} , \"script_fields\" : { \"test1\" : { \"script_id\" : \"script1\", \"lang\":\"groovy\" }, \"test2\" : { \"script_id\" : \"script2\", \"lang\":\"groovy\", \"params\":{\"factor\":3}  }}, size:1}"
 decl_stmt|;
 name|searchResponse
 operator|=
