3659f1c22db703935663e5ad6242904a46310e45
Areek Zillur <areek.zillur@elasticsearch.com>
2014-08-08 11:49:21 -0400

    Suggester: add suggestRequest to Requests and fix broken javadocs in client
    closes #7206
    
    
    Former-commit-id: 99ae3066de6575694298eeb1079229645af3971b

--- src/main/java/org/elasticsearch/action/suggest/SuggestRequest.java
+++ src/main/java/org/elasticsearch/action/suggest/SuggestRequest.java
@@ -189,7 +189,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * A request to get suggestions for corrections of phrases. Best created with  * {@link org.elasticsearch.client.Requests#suggestRequest(String...)}.  *<p/>  *<p>The request requires the query source to be set either using {@link #query(org.elasticsearch.index.query.QueryBuilder)},  * or {@link #query(byte[])}.  *  * @see SuggestResponse  * @see org.elasticsearch.client.Client#suggest(SuggestRequest)  * @see org.elasticsearch.client.Requests#suggestRequest(String...)  */
+comment|/**  * A request to get suggestions for corrections of phrases. Best created with  * {@link org.elasticsearch.client.Requests#suggestRequest(String...)}.  *<p/>  *<p>The request requires the suggest query source to be set either using {@link #suggest(org.elasticsearch.common.bytes.BytesReference)},  * or {@link #suggest(org.elasticsearch.common.bytes.BytesReference, boolean)}.  *  * @see SuggestResponse  * @see org.elasticsearch.client.Client#suggest(SuggestRequest)  * @see org.elasticsearch.client.Requests#suggestRequest(String...)  */
 end_comment
 
 begin_class
