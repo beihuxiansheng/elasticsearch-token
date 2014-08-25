beihuxiansheng/elasticsearch-token

commit b726267093bb44884ac014ee797697214ccb1ac1

Tests: Improve test coverage.
Close #7428


Former-commit-id: 6950c38a0436ec937797f01fba8d7d95e6d6225f

src/main/java/org/elasticsearch/indices/fielddata/cache/IndicesFieldDataCache.java
@@ -208,7 +208,49 @@ name|index
 operator|.
 name|fielddata
 operator|.
-name|*
+name|AtomicFieldData
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|elasticsearch
+operator|.
+name|index
+operator|.
+name|fielddata
+operator|.
+name|FieldDataType
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|elasticsearch
+operator|.
+name|index
+operator|.
+name|fielddata
+operator|.
+name|IndexFieldData
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|elasticsearch
+operator|.
+name|index
+operator|.
+name|fielddata
+operator|.
+name|IndexFieldDataCache
 import|;
 end_import
 
@@ -816,13 +858,20 @@ name|getValue
 argument_list|()
 decl_stmt|;
 assert|assert
-name|sizeInBytes
-operator|>=
-literal|0
-operator|||
 name|value
-operator|!=
+operator|==
 literal|null
+operator|||
+name|sizeInBytes
+operator|>
+literal|0
+operator|&&
+name|sizeInBytes
+operator|==
+name|value
+operator|.
+name|ramBytesUsed
+argument_list|()
 operator|:
 literal|"Expected size ["
 operator|+
@@ -834,26 +883,6 @@ name|value
 operator|+
 literal|"] to be non-null"
 assert|;
-if|if
-condition|(
-name|sizeInBytes
-operator|==
-operator|-
-literal|1
-operator|&&
-name|value
-operator|!=
-literal|null
-condition|)
-block|{
-name|sizeInBytes
-operator|=
-name|value
-operator|.
-name|ramBytesUsed
-argument_list|()
-expr_stmt|;
-block|}
 for|for
 control|(
 name|IndexFieldDataCache
@@ -1550,6 +1579,15 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
+name|key
+operator|.
+name|sizeInBytes
+operator|=
+name|ifd
+operator|.
+name|ramBytesUsed
+argument_list|()
+expr_stmt|;
 return|return
 name|ifd
 return|;

rest-api-spec/test/cat.indices/10_basic.yml
@@ -1,0 +1,26 @@
+---
+"Test cat indices output":
+
+  - do:
+      cat.indices: {}
+
+  - match:
+      $body: |
+               /^$/
+
+  - do:
+      indices.create:
+        index: index1
+        body:
+          settings:
+            number_of_shards: "1"
+            number_of_replicas: "0"
+  - do:
+       cluster.health:
+         wait_for_status: yellow
+  - do:
+      cat.indices: {}
+
+  - match:
+      $body: |
+               /^(green \s+ index1 \s+ 1 \s+ 0 \s+ 0 \s+ 0 \s+ (\d+|\d+[.]\d+)(kb|b) \s+ (\d+|\d+[.]\d+)(kb|b))$/

src/main/java/org/elasticsearch/common/Booleans.java
@@ -24,6 +24,7 @@ specifier|public
 class|class
 name|Booleans
 block|{
+comment|/**      * Returns<code>true</code> iff the sequence is neither of the following:      *<tt>false</tt>,<tt>0</tt>,<tt>off</tt>,<tt>no</tt>,      *   otherwise<code>false</code>      */
 DECL|method|parseBoolean
 specifier|public
 specifier|static
@@ -469,13 +470,13 @@ block|}
 DECL|method|parseBoolean
 specifier|public
 specifier|static
-name|boolean
+name|Boolean
 name|parseBoolean
 parameter_list|(
 name|String
 name|value
 parameter_list|,
-name|boolean
+name|Boolean
 name|defaultValue
 parameter_list|)
 block|{
@@ -486,53 +487,31 @@ operator|==
 literal|null
 condition|)
 block|{
+comment|// only for the null case we do that here!
 return|return
 name|defaultValue
 return|;
 block|}
 return|return
-operator|!
-operator|(
-name|value
-operator|.
-name|equals
+name|parseBoolean
 argument_list|(
-literal|"false"
-argument_list|)
-operator|||
 name|value
-operator|.
-name|equals
-argument_list|(
-literal|"0"
+argument_list|,
+literal|false
 argument_list|)
-operator|||
-name|value
-operator|.
-name|equals
-argument_list|(
-literal|"off"
-argument_list|)
-operator|||
-name|value
-operator|.
-name|equals
-argument_list|(
-literal|"no"
-argument_list|)
-operator|)
 return|;
 block|}
+comment|/**      * Returns<code>true</code> iff the value is neither of the following:      *<tt>false</tt>,<tt>0</tt>,<tt>off</tt>,<tt>no</tt>      *   otherwise<code>false</code>      */
 DECL|method|parseBoolean
 specifier|public
 specifier|static
-name|Boolean
+name|boolean
 name|parseBoolean
 parameter_list|(
 name|String
 name|value
 parameter_list|,
-name|Boolean
+name|boolean
 name|defaultValue
 parameter_list|)
 block|{
@@ -580,6 +559,7 @@ argument_list|)
 operator|)
 return|;
 block|}
+comment|/**      * Returns<code>true</code> iff the value is either of the following:      *<tt>false</tt>,<tt>0</tt>,<tt>off</tt>,<tt>no</tt>      *   otherwise<code>false</code>      */
 DECL|method|isExplicitFalse
 specifier|public
 specifier|static
@@ -591,6 +571,10 @@ name|value
 parameter_list|)
 block|{
 return|return
+name|value
+operator|!=
+literal|null
+operator|&&
 operator|(
 name|value
 operator|.
@@ -622,6 +606,7 @@ argument_list|)
 operator|)
 return|;
 block|}
+comment|/**      * Returns<code>true</code> iff the value is either of the following:      *<tt>true</tt>,<tt>1</tt>,<tt>on</tt>,<tt>yes</tt>      *   otherwise<code>false</code>      */
 DECL|method|isExplicitTrue
 specifier|public
 specifier|static
@@ -633,6 +618,10 @@ name|value
 parameter_list|)
 block|{
 return|return
+name|value
+operator|!=
+literal|null
+operator|&&
 operator|(
 name|value
 operator|.

src/main/java/org/elasticsearch/common/Preconditions.java
@@ -50,16 +50,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|Collection
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|NoSuchElementException
 import|;
 end_import
@@ -389,492 +379,6 @@ block|}
 return|return
 name|reference
 return|;
-block|}
-comment|/**      * Ensures that an {@code Iterable} object passed as a parameter to the      * calling method is not null and contains no null elements.      *      * @param iterable the iterable to check the contents of      * @return the non-null {@code iterable} reference just validated      * @throws org.elasticsearch.ElasticsearchNullPointerException      *          if {@code iterable} is null or contains at      *          least one null element      */
-DECL|method|checkContentsNotNull
-specifier|public
-specifier|static
-parameter_list|<
-name|T
-extends|extends
-name|Iterable
-argument_list|<
-name|?
-argument_list|>
-parameter_list|>
-name|T
-name|checkContentsNotNull
-parameter_list|(
-name|T
-name|iterable
-parameter_list|)
-block|{
-if|if
-condition|(
-name|containsOrIsNull
-argument_list|(
-name|iterable
-argument_list|)
-condition|)
-block|{
-throw|throw
-operator|new
-name|ElasticsearchNullPointerException
-argument_list|()
-throw|;
-block|}
-return|return
-name|iterable
-return|;
-block|}
-comment|/**      * Ensures that an {@code Iterable} object passed as a parameter to the      * calling method is not null and contains no null elements.      *      * @param iterable     the iterable to check the contents of      * @param errorMessage the exception message to use if the check fails; will      *                     be converted to a string using {@link String#valueOf(Object)}      * @return the non-null {@code iterable} reference just validated      * @throws org.elasticsearch.ElasticsearchNullPointerException      *          if {@code iterable} is null or contains at      *          least one null element      */
-DECL|method|checkContentsNotNull
-specifier|public
-specifier|static
-parameter_list|<
-name|T
-extends|extends
-name|Iterable
-argument_list|<
-name|?
-argument_list|>
-parameter_list|>
-name|T
-name|checkContentsNotNull
-parameter_list|(
-name|T
-name|iterable
-parameter_list|,
-name|Object
-name|errorMessage
-parameter_list|)
-block|{
-if|if
-condition|(
-name|containsOrIsNull
-argument_list|(
-name|iterable
-argument_list|)
-condition|)
-block|{
-throw|throw
-operator|new
-name|ElasticsearchNullPointerException
-argument_list|(
-name|String
-operator|.
-name|valueOf
-argument_list|(
-name|errorMessage
-argument_list|)
-argument_list|)
-throw|;
-block|}
-return|return
-name|iterable
-return|;
-block|}
-comment|/**      * Ensures that an {@code Iterable} object passed as a parameter to the      * calling method is not null and contains no null elements.      *      * @param iterable             the iterable to check the contents of      * @param errorMessageTemplate a template for the exception message should the      *                             check fail. The message is formed by replacing each {@code %s}      *                             placeholder in the template with an argument. These are matched by      *                             position - the first {@code %s} gets {@code errorMessageArgs[0]}, etc.      *                             Unmatched arguments will be appended to the formatted message in square      *                             braces. Unmatched placeholders will be left as-is.      * @param errorMessageArgs     the arguments to be substituted into the message      *                             template. Arguments are converted to strings using      *                             {@link String#valueOf(Object)}.      * @return the non-null {@code iterable} reference just validated      * @throws org.elasticsearch.ElasticsearchNullPointerException      *          if {@code iterable} is null or contains at      *          least one null element      */
-DECL|method|checkContentsNotNull
-specifier|public
-specifier|static
-parameter_list|<
-name|T
-extends|extends
-name|Iterable
-argument_list|<
-name|?
-argument_list|>
-parameter_list|>
-name|T
-name|checkContentsNotNull
-parameter_list|(
-name|T
-name|iterable
-parameter_list|,
-name|String
-name|errorMessageTemplate
-parameter_list|,
-name|Object
-modifier|...
-name|errorMessageArgs
-parameter_list|)
-block|{
-if|if
-condition|(
-name|containsOrIsNull
-argument_list|(
-name|iterable
-argument_list|)
-condition|)
-block|{
-throw|throw
-operator|new
-name|ElasticsearchNullPointerException
-argument_list|(
-name|format
-argument_list|(
-name|errorMessageTemplate
-argument_list|,
-name|errorMessageArgs
-argument_list|)
-argument_list|)
-throw|;
-block|}
-return|return
-name|iterable
-return|;
-block|}
-DECL|method|containsOrIsNull
-specifier|private
-specifier|static
-name|boolean
-name|containsOrIsNull
-parameter_list|(
-name|Iterable
-argument_list|<
-name|?
-argument_list|>
-name|iterable
-parameter_list|)
-block|{
-if|if
-condition|(
-name|iterable
-operator|==
-literal|null
-condition|)
-block|{
-return|return
-literal|true
-return|;
-block|}
-if|if
-condition|(
-name|iterable
-operator|instanceof
-name|Collection
-condition|)
-block|{
-name|Collection
-argument_list|<
-name|?
-argument_list|>
-name|collection
-init|=
-operator|(
-name|Collection
-argument_list|<
-name|?
-argument_list|>
-operator|)
-name|iterable
-decl_stmt|;
-try|try
-block|{
-return|return
-name|collection
-operator|.
-name|contains
-argument_list|(
-literal|null
-argument_list|)
-return|;
-block|}
-catch|catch
-parameter_list|(
-name|ElasticsearchNullPointerException
-name|e
-parameter_list|)
-block|{
-comment|// A NPE implies that the collection doesn't contain null.
-return|return
-literal|false
-return|;
-block|}
-block|}
-else|else
-block|{
-for|for
-control|(
-name|Object
-name|element
-range|:
-name|iterable
-control|)
-block|{
-if|if
-condition|(
-name|element
-operator|==
-literal|null
-condition|)
-block|{
-return|return
-literal|true
-return|;
-block|}
-block|}
-return|return
-literal|false
-return|;
-block|}
-block|}
-comment|/**      * Ensures that {@code index} specifies a valid<i>element</i> in an array,      * list or string of size {@code size}. An element index may range from zero,      * inclusive, to {@code size}, exclusive.      *      * @param index a user-supplied index identifying an element of an array, list      *              or string      * @param size  the size of that array, list or string      * @throws IndexOutOfBoundsException if {@code index} is negative or is not      *                                   less than {@code size}      * @throws org.elasticsearch.ElasticsearchIllegalArgumentException      *                                   if {@code size} is negative      */
-DECL|method|checkElementIndex
-specifier|public
-specifier|static
-name|void
-name|checkElementIndex
-parameter_list|(
-name|int
-name|index
-parameter_list|,
-name|int
-name|size
-parameter_list|)
-block|{
-name|checkElementIndex
-argument_list|(
-name|index
-argument_list|,
-name|size
-argument_list|,
-literal|"index"
-argument_list|)
-expr_stmt|;
-block|}
-comment|/**      * Ensures that {@code index} specifies a valid<i>element</i> in an array,      * list or string of size {@code size}. An element index may range from zero,      * inclusive, to {@code size}, exclusive.      *      * @param index a user-supplied index identifying an element of an array, list      *              or string      * @param size  the size of that array, list or string      * @param desc  the text to use to describe this index in an error message      * @throws IndexOutOfBoundsException if {@code index} is negative or is not      *                                   less than {@code size}      * @throws org.elasticsearch.ElasticsearchIllegalArgumentException      *                                   if {@code size} is negative      */
-DECL|method|checkElementIndex
-specifier|public
-specifier|static
-name|void
-name|checkElementIndex
-parameter_list|(
-name|int
-name|index
-parameter_list|,
-name|int
-name|size
-parameter_list|,
-name|String
-name|desc
-parameter_list|)
-block|{
-name|checkArgument
-argument_list|(
-name|size
-operator|>=
-literal|0
-argument_list|,
-literal|"negative size: %s"
-argument_list|,
-name|size
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|index
-operator|<
-literal|0
-condition|)
-block|{
-throw|throw
-operator|new
-name|IndexOutOfBoundsException
-argument_list|(
-name|format
-argument_list|(
-literal|"%s (%s) must not be negative"
-argument_list|,
-name|desc
-argument_list|,
-name|index
-argument_list|)
-argument_list|)
-throw|;
-block|}
-if|if
-condition|(
-name|index
-operator|>=
-name|size
-condition|)
-block|{
-throw|throw
-operator|new
-name|IndexOutOfBoundsException
-argument_list|(
-name|format
-argument_list|(
-literal|"%s (%s) must be less than size (%s)"
-argument_list|,
-name|desc
-argument_list|,
-name|index
-argument_list|,
-name|size
-argument_list|)
-argument_list|)
-throw|;
-block|}
-block|}
-comment|/**      * Ensures that {@code index} specifies a valid<i>position</i> in an array,      * list or string of size {@code size}. A position index may range from zero      * to {@code size}, inclusive.      *      * @param index a user-supplied index identifying a position in an array, list      *              or string      * @param size  the size of that array, list or string      * @throws IndexOutOfBoundsException if {@code index} is negative or is      *                                   greater than {@code size}      * @throws org.elasticsearch.ElasticsearchIllegalArgumentException      *                                   if {@code size} is negative      */
-DECL|method|checkPositionIndex
-specifier|public
-specifier|static
-name|void
-name|checkPositionIndex
-parameter_list|(
-name|int
-name|index
-parameter_list|,
-name|int
-name|size
-parameter_list|)
-block|{
-name|checkPositionIndex
-argument_list|(
-name|index
-argument_list|,
-name|size
-argument_list|,
-literal|"index"
-argument_list|)
-expr_stmt|;
-block|}
-comment|/**      * Ensures that {@code index} specifies a valid<i>position</i> in an array,      * list or string of size {@code size}. A position index may range from zero      * to {@code size}, inclusive.      *      * @param index a user-supplied index identifying a position in an array, list      *              or string      * @param size  the size of that array, list or string      * @param desc  the text to use to describe this index in an error message      * @throws IndexOutOfBoundsException if {@code index} is negative or is      *                                   greater than {@code size}      * @throws org.elasticsearch.ElasticsearchIllegalArgumentException      *                                   if {@code size} is negative      */
-DECL|method|checkPositionIndex
-specifier|public
-specifier|static
-name|void
-name|checkPositionIndex
-parameter_list|(
-name|int
-name|index
-parameter_list|,
-name|int
-name|size
-parameter_list|,
-name|String
-name|desc
-parameter_list|)
-block|{
-name|checkArgument
-argument_list|(
-name|size
-operator|>=
-literal|0
-argument_list|,
-literal|"negative size: %s"
-argument_list|,
-name|size
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|index
-operator|<
-literal|0
-condition|)
-block|{
-throw|throw
-operator|new
-name|IndexOutOfBoundsException
-argument_list|(
-name|format
-argument_list|(
-literal|"%s (%s) must not be negative"
-argument_list|,
-name|desc
-argument_list|,
-name|index
-argument_list|)
-argument_list|)
-throw|;
-block|}
-if|if
-condition|(
-name|index
-operator|>
-name|size
-condition|)
-block|{
-throw|throw
-operator|new
-name|IndexOutOfBoundsException
-argument_list|(
-name|format
-argument_list|(
-literal|"%s (%s) must not be greater than size (%s)"
-argument_list|,
-name|desc
-argument_list|,
-name|index
-argument_list|,
-name|size
-argument_list|)
-argument_list|)
-throw|;
-block|}
-block|}
-comment|/**      * Ensures that {@code start} and {@code end} specify a valid<i>positions</i>      * in an array, list or string of size {@code size}, and are in order. A      * position index may range from zero to {@code size}, inclusive.      *      * @param start a user-supplied index identifying a starting position in an      *              array, list or string      * @param end   a user-supplied index identifying a ending position in an array,      *              list or string      * @param size  the size of that array, list or string      * @throws IndexOutOfBoundsException if either index is negative or is      *                                   greater than {@code size}, or if {@code end} is less than {@code start}      * @throws org.elasticsearch.ElasticsearchIllegalArgumentException      *                                   if {@code size} is negative      */
-DECL|method|checkPositionIndexes
-specifier|public
-specifier|static
-name|void
-name|checkPositionIndexes
-parameter_list|(
-name|int
-name|start
-parameter_list|,
-name|int
-name|end
-parameter_list|,
-name|int
-name|size
-parameter_list|)
-block|{
-name|checkPositionIndex
-argument_list|(
-name|start
-argument_list|,
-name|size
-argument_list|,
-literal|"start index"
-argument_list|)
-expr_stmt|;
-name|checkPositionIndex
-argument_list|(
-name|end
-argument_list|,
-name|size
-argument_list|,
-literal|"end index"
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|end
-operator|<
-name|start
-condition|)
-block|{
-throw|throw
-operator|new
-name|IndexOutOfBoundsException
-argument_list|(
-name|format
-argument_list|(
-literal|"end index (%s) must not be less than start index (%s)"
-argument_list|,
-name|end
-argument_list|,
-name|start
-argument_list|)
-argument_list|)
-throw|;
-block|}
 block|}
 comment|/**      * Substitutes each {@code %s} in {@code template} with an argument. These      * are matched by position - the first {@code %s} gets {@code args[0]}, etc.      * If there are more arguments than placeholders, the unmatched arguments will      * be appended to the end of the formatted message in square braces.      *      * @param template a non-null string containing 0 or more {@code %s}      *                 placeholders.      * @param args     the arguments to be substituted into the message      *                 template. Arguments are converted to strings using      *                 {@link String#valueOf(Object)}. Arguments can be null.      */
 comment|// VisibleForTesting

src/main/java/org/elasticsearch/common/geo/GeoHashUtils.java
@@ -46,16 +46,6 @@ name|Collection
 import|;
 end_import
 
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
-name|Iterator
-import|;
-end_import
-
 begin_comment
 comment|/**  * Utilities for encoding and decoding geohashes. Based on  * http://en.wikipedia.org/wiki/Geohash.  */
 end_comment
@@ -521,50 +511,6 @@ argument_list|(
 literal|8
 argument_list|)
 argument_list|)
-return|;
-block|}
-comment|/**      * Create an {@link Iterable} which allows to iterate over the cells that      * contain a given geohash      *         * @param geohash Geohash of a cell      *       * @return {@link Iterable} of path      */
-DECL|method|path
-specifier|public
-specifier|static
-name|Iterable
-argument_list|<
-name|String
-argument_list|>
-name|path
-parameter_list|(
-specifier|final
-name|String
-name|geohash
-parameter_list|)
-block|{
-return|return
-operator|new
-name|Iterable
-argument_list|<
-name|String
-argument_list|>
-argument_list|()
-block|{
-annotation|@
-name|Override
-specifier|public
-name|Iterator
-argument_list|<
-name|String
-argument_list|>
-name|iterator
-parameter_list|()
-block|{
-return|return
-operator|new
-name|GeohashPathIterator
-argument_list|(
-name|geohash
-argument_list|)
-return|;
-block|}
-block|}
 return|;
 block|}
 comment|/**      * Calculate the geohash of a neighbor of a geohash      *      * @param geohash the geohash of a cell      * @param level   level of the geohash      * @param dx      delta of the first grid coordinate (must be -1, 0 or +1)      * @param dy      delta of the second grid coordinate (must be -1, 0 or +1)      * @return geohash of the defined cell      */
@@ -1451,63 +1397,6 @@ operator|/
 literal|2D
 argument_list|)
 return|;
-block|}
-comment|/**      * Decodes the given geohash into a geohash cell defined by the points nothWest and southEast      *      * @param geohash   Geohash to deocde      * @param northWest the point north/west of the cell      * @param southEast the point south/east of the cell      */
-DECL|method|decodeCell
-specifier|public
-specifier|static
-name|void
-name|decodeCell
-parameter_list|(
-name|String
-name|geohash
-parameter_list|,
-name|GeoPoint
-name|northWest
-parameter_list|,
-name|GeoPoint
-name|southEast
-parameter_list|)
-block|{
-name|double
-index|[]
-name|interval
-init|=
-name|decodeCell
-argument_list|(
-name|geohash
-argument_list|)
-decl_stmt|;
-name|northWest
-operator|.
-name|reset
-argument_list|(
-name|interval
-index|[
-literal|1
-index|]
-argument_list|,
-name|interval
-index|[
-literal|2
-index|]
-argument_list|)
-expr_stmt|;
-name|southEast
-operator|.
-name|reset
-argument_list|(
-name|interval
-index|[
-literal|0
-index|]
-argument_list|,
-name|interval
-index|[
-literal|3
-index|]
-argument_list|)
-expr_stmt|;
 block|}
 DECL|method|decodeCell
 specifier|private

src/main/java/org/elasticsearch/common/geo/GeoPoint.java
@@ -249,30 +249,6 @@ return|return
 name|this
 return|;
 block|}
-DECL|method|latlon
-name|void
-name|latlon
-parameter_list|(
-name|double
-name|lat
-parameter_list|,
-name|double
-name|lon
-parameter_list|)
-block|{
-name|this
-operator|.
-name|lat
-operator|=
-name|lat
-expr_stmt|;
-name|this
-operator|.
-name|lon
-operator|=
-name|lon
-expr_stmt|;
-block|}
 DECL|method|lat
 specifier|public
 specifier|final

src/main/java/org/elasticsearch/common/geo/GeoUtils.java
@@ -1841,6 +1841,11 @@ argument_list|)
 throw|;
 block|}
 block|}
+DECL|method|GeoUtils
+specifier|private
+name|GeoUtils
+parameter_list|()
+block|{     }
 block|}
 end_class
 

commit 6eee25450a0f2883c2eda6c53f0deb67c588c228

[TEST] fix issue clearing fielddata breaker introduced in b726267093bb44884ac014ee797697214ccb1ac1 [formerly 6950c38a0436ec937797f01fba8d7d95e6d6225f]
Former-commit-id: 1f7be7931bcee59d45b59fd98827c860e41e3e50

src/main/java/org/elasticsearch/indices/fielddata/cache/IndicesFieldDataCache.java
@@ -858,20 +858,13 @@ name|getValue
 argument_list|()
 decl_stmt|;
 assert|assert
-name|value
-operator|==
-literal|null
-operator|||
 name|sizeInBytes
-operator|>
+operator|>=
 literal|0
-operator|&&
-name|sizeInBytes
-operator|==
+operator|||
 name|value
-operator|.
-name|ramBytesUsed
-argument_list|()
+operator|!=
+literal|null
 operator|:
 literal|"Expected size ["
 operator|+
@@ -883,6 +876,26 @@ name|value
 operator|+
 literal|"] to be non-null"
 assert|;
+if|if
+condition|(
+name|sizeInBytes
+operator|==
+operator|-
+literal|1
+operator|&&
+name|value
+operator|!=
+literal|null
+condition|)
+block|{
+name|sizeInBytes
+operator|=
+name|value
+operator|.
+name|ramBytesUsed
+argument_list|()
+expr_stmt|;
+block|}
 for|for
 control|(
 name|IndexFieldDataCache
@@ -1579,15 +1592,6 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
-name|key
-operator|.
-name|sizeInBytes
-operator|=
-name|ifd
-operator|.
-name|ramBytesUsed
-argument_list|()
-expr_stmt|;
 return|return
 name|ifd
 return|;