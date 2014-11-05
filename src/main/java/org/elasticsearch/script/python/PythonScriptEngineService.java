d1b4fdd4f99115706f28650175d76d6ef3e349ce
Robert Muir <rmuir@apache.org>
2014-11-05 17:18:37 -0500

    upgrade to lucene 5 snapshot
    Former-commit-id: 8cbdf5302f7caebe0d168fd9e5c4212d8d21bb89

--- src/main/java/org/elasticsearch/script/python/PythonScriptEngineService.java
+++ src/main/java/org/elasticsearch/script/python/PythonScriptEngineService.java
@@ -26,7 +26,7 @@ name|lucene
 operator|.
 name|index
 operator|.
-name|AtomicReaderContext
+name|LeafReaderContext
 import|;
 end_import
 
@@ -887,7 +887,7 @@ specifier|public
 name|void
 name|setNextReader
 parameter_list|(
-name|AtomicReaderContext
+name|LeafReaderContext
 name|context
 parameter_list|)
 block|{
