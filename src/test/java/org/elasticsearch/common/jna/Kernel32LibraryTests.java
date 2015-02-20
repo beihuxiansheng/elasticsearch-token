f508e48f4c9d9760d8d589f5cd9ddcc8d53a0250
Robert Muir <rmuir@apache.org>
2015-02-20 17:58:56 -0500

    set AwaitsFix until we discuss if proxies are needed
    Former-commit-id: a4f7023e2900ff309de80a15bf56b1a78c9a8506

--- src/test/java/org/elasticsearch/common/jna/Kernel32LibraryTests.java
+++ src/test/java/org/elasticsearch/common/jna/Kernel32LibraryTests.java
@@ -34,6 +34,22 @@ begin_import
 import|import
 name|org
 operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|util
+operator|.
+name|LuceneTestCase
+operator|.
+name|AwaitsFix
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
 name|elasticsearch
 operator|.
 name|common
@@ -121,6 +137,13 @@ import|;
 end_import
 
 begin_class
+annotation|@
+name|AwaitsFix
+argument_list|(
+name|bugUrl
+operator|=
+literal|"https://github.com/elasticsearch/elasticsearch/issues/9802"
+argument_list|)
 DECL|class|Kernel32LibraryTests
 specifier|public
 class|class
