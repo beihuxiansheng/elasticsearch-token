53c45ed01705c577ca1082c6263ec996ce3c5e7b
javanna <cavannaluca@gmail.com>
2015-04-28 14:24:51 +0200

    [TEST] make CountRequestBuilder#toString and SearchRequestBuilder#toString tests non platform dependent
    Former-commit-id: b8caa52e7c329e679935e26f554e7180df7b16d9

--- src/test/java/org/elasticsearch/action/search/SearchRequestBuilderTests.java
+++ src/test/java/org/elasticsearch/action/search/SearchRequestBuilderTests.java
@@ -186,6 +186,18 @@ name|hamcrest
 operator|.
 name|CoreMatchers
 operator|.
+name|containsString
+import|;
+end_import
+
+begin_import
+import|import static
+name|org
+operator|.
+name|hamcrest
+operator|.
+name|CoreMatchers
+operator|.
 name|equalTo
 import|;
 end_import
@@ -455,9 +467,9 @@ operator|.
 name|toString
 argument_list|()
 argument_list|,
-name|equalTo
+name|containsString
 argument_list|(
-literal|"{\n  \"query\":{ \"match_all\" : {} }\n}"
+literal|"\"query\":{ \"match_all\" : {} }"
 argument_list|)
 argument_list|)
 expr_stmt|;
