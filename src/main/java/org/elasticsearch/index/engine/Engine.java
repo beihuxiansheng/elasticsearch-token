45f898c3f8dc1694f7d8170a147951642a5011d0
Shay Banon <kimchy@gmail.com>
2015-04-29 11:37:32 +0200

    Remove Preconditions class We don't need our own anymore, since we removed the Elasticsearch illegal argument exception
    Former-commit-id: 11bf7074d77dcfb5ea9b9916829c0f53361e8ce6

--- src/main/java/org/elasticsearch/index/engine/Engine.java
+++ src/main/java/org/elasticsearch/index/engine/Engine.java
@@ -18,6 +18,20 @@ end_package
 
 begin_import
 import|import
+name|com
+operator|.
+name|google
+operator|.
+name|common
+operator|.
+name|base
+operator|.
+name|Preconditions
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -136,16 +150,6 @@ name|org
 operator|.
 name|elasticsearch
 operator|.
-name|ElasticsearchException
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|elasticsearch
-operator|.
 name|ExceptionsHelper
 import|;
 end_import
@@ -159,18 +163,6 @@ operator|.
 name|common
 operator|.
 name|Nullable
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|elasticsearch
-operator|.
-name|common
-operator|.
-name|Preconditions
 import|;
 end_import
 
