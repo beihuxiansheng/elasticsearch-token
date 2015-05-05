f48cbf6dd7adc898be2ccfb7d2366247e4cbb54c
Robert Muir <rmuir@apache.org>
2015-05-05 12:47:37 -0400

    Tests: fix tests not to use CWD
    Former-commit-id: 7c05b8ebf522bb451437bc7054d400761551a974

--- src/test/java/org/elasticsearch/index/analysis/KuromojiAnalysisTests.java
+++ src/test/java/org/elasticsearch/index/analysis/KuromojiAnalysisTests.java
@@ -1405,6 +1405,14 @@ operator|.
 name|settingsBuilder
 argument_list|()
 operator|.
+name|put
+argument_list|(
+literal|"path.home"
+argument_list|,
+name|createTempDir
+argument_list|()
+argument_list|)
+operator|.
 name|loadFromClasspath
 argument_list|(
 literal|"org/elasticsearch/index/analysis/kuromoji_analysis.json"
