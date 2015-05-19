75193f0843780d2168a515e6799a7e518041a684
Simon Willnauer <simonw@apache.org>
2015-05-19 14:10:07 +0200

    [ENGINE] Remove flushNeeded in favor of IW#hasUncommittedChanges()
    We maintain a boolean that is obsolete since IW maintains the same
    property that we can just piggyback on.
    
    
    Former-commit-id: d6e78176665c49c5348cd2022f3e61c042be08d3

--- src/main/java/org/elasticsearch/index/engine/InternalEngine.java
+++ src/main/java/org/elasticsearch/index/engine/InternalEngine.java
@@ -769,16 +769,6 @@ specifier|final
 name|SearcherManager
 name|searcherManager
 decl_stmt|;
-comment|// we use flushNeeded here, since if there are no changes, then the commit won't write
-comment|// will not really happen, and then the commitUserData and the new translog will not be reflected
-DECL|field|flushNeeded
-specifier|private
-specifier|volatile
-name|boolean
-name|flushNeeded
-init|=
-literal|false
-decl_stmt|;
 DECL|field|flushLock
 specifier|private
 specifier|final
@@ -2235,10 +2225,6 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
-name|flushNeeded
-operator|=
-literal|true
-expr_stmt|;
 block|}
 catch|catch
 parameter_list|(
@@ -2929,10 +2915,6 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
-name|flushNeeded
-operator|=
-literal|true
-expr_stmt|;
 block|}
 catch|catch
 parameter_list|(
@@ -3508,10 +3490,6 @@ name|innerDelete
 argument_list|(
 name|delete
 argument_list|)
-expr_stmt|;
-name|flushNeeded
-operator|=
-literal|true
 expr_stmt|;
 block|}
 catch|catch
@@ -4094,10 +4072,6 @@ name|delete
 argument_list|)
 argument_list|)
 expr_stmt|;
-name|flushNeeded
-operator|=
-literal|true
-expr_stmt|;
 block|}
 catch|catch
 parameter_list|(
@@ -4352,19 +4326,14 @@ try|try
 block|{
 if|if
 condition|(
-name|flushNeeded
+name|indexWriter
+operator|.
+name|hasUncommittedChanges
+argument_list|()
 operator|||
 name|force
 condition|)
 block|{
-name|flushNeeded
-operator|=
-literal|false
-expr_stmt|;
-specifier|final
-name|long
-name|translogId
-decl_stmt|;
 try|try
 block|{
 name|translog
