20d0a37b3519dd43bb9ab727e4f098e4975abfb5
Simon Willnauer <simonw@apache.org>
2015-05-29 13:52:26 +0200

    Consolidate shard level modules without logic into IndexShardModule
    We have a lot of module classes that don't contain any actual logic,
    only declarative bind actions. These classes are unnecessary and can
    be consolidated into the already existings IndexShardModule
    
    
    Former-commit-id: 5a9694783b657e58ee2c3e3c84b9696a0fdd4eeb

--- src/main/java/org/elasticsearch/cluster/metadata/MetaDataCreateIndexService.java
+++ src/main/java/org/elasticsearch/cluster/metadata/MetaDataCreateIndexService.java
@@ -798,12 +798,6 @@ operator|new
 name|DefaultIndexTemplateFilter
 argument_list|()
 decl_stmt|;
-DECL|field|environment
-specifier|private
-specifier|final
-name|Environment
-name|environment
-decl_stmt|;
 DECL|field|threadPool
 specifier|private
 specifier|final
@@ -873,9 +867,6 @@ parameter_list|(
 name|Settings
 name|settings
 parameter_list|,
-name|Environment
-name|environment
-parameter_list|,
 name|ThreadPool
 name|threadPool
 parameter_list|,
@@ -916,12 +907,6 @@ name|super
 argument_list|(
 name|settings
 argument_list|)
-expr_stmt|;
-name|this
-operator|.
-name|environment
-operator|=
-name|environment
 expr_stmt|;
 name|this
 operator|.
