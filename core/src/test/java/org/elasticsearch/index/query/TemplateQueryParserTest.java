d5bb59e8947614274a7d40b9492a61a2a671ff6f
javanna <cavannaluca@gmail.com>
2015-06-03 10:45:29 +0200

    Plugins: one single (global) way to register custom query parsers
    There are different ways to register custom query parsers through plugins, a couple of them work per index via index settings, which is probably even too flexible. There also three different ways to add a global custom query parser through either IndicesQueriesModule or IndicesQueriesRegistry. This commit consolidates the registration of custom query parsers via IndicesQueriesModule#addQuery(Class<? extends QueryParser>). The complexity of supporting parsers per index is not needed hence it got removed. Also the other ways of registering global custom parsers are dropped in favour of the one mentioned above.
    
    Closes #11481
    
    
    Former-commit-id: 2ef0fcfd6ad16942d957e0162fa8ca2bd25e500b

--- core/src/test/java/org/elasticsearch/index/query/TemplateQueryParserTest.java
+++ core/src/test/java/org/elasticsearch/index/query/TemplateQueryParserTest.java
@@ -622,12 +622,6 @@ name|index
 argument_list|)
 argument_list|,
 operator|new
-name|IndexQueryParserModule
-argument_list|(
-name|settings
-argument_list|)
-argument_list|,
-operator|new
 name|FunctionScoreModule
 argument_list|()
 argument_list|,
