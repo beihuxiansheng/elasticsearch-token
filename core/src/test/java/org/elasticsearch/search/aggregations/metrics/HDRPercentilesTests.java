6eb6ba13930a06dbe09300ec09e201cfecb343ca
Colin Goodheart-Smithe <colings86@users.noreply.github.com>
2015-07-28 10:17:47 +0100

    [Test] Muted all HDRPercentilesTests and HDRPercentileRanksTests
    Former-commit-id: 7ab9bbbfad8d1ca221789529ed062d6f77d53f0c

--- core/src/test/java/org/elasticsearch/search/aggregations/metrics/HDRPercentilesTests.java
+++ core/src/test/java/org/elasticsearch/search/aggregations/metrics/HDRPercentilesTests.java
@@ -36,6 +36,22 @@ begin_import
 import|import
 name|org
 operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|util
+operator|.
+name|LuceneTestCase
+operator|.
+name|AwaitsFix
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
 name|elasticsearch
 operator|.
 name|action
@@ -387,6 +403,13 @@ comment|/**  *  */
 end_comment
 
 begin_class
+annotation|@
+name|AwaitsFix
+argument_list|(
+name|bugUrl
+operator|=
+literal|"single test methods fail with occassional seeds (see testScript_ExplicitSingleValued_WithParams for example) but only if run as a whole test class not if run as a single test method"
+argument_list|)
 DECL|class|HDRPercentilesTests
 specifier|public
 class|class
