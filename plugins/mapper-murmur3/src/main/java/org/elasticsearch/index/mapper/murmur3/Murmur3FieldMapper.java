ec88bbf6c1ff56eef64c48b33ed1b2cb6329c868
Adrien Grand <jpountz@gmail.com>
2015-08-18 12:30:51 +0200

    Fix mapper-murmur3 compatibility version.
    Former-commit-id: a72adbf0b4c4acaaa60635de44d99a82f9ffd3a1

--- plugins/mapper-murmur3/src/main/java/org/elasticsearch/index/mapper/murmur3/Murmur3FieldMapper.java
+++ plugins/mapper-murmur3/src/main/java/org/elasticsearch/index/mapper/murmur3/Murmur3FieldMapper.java
@@ -466,7 +466,7 @@ name|onOrAfter
 argument_list|(
 name|Version
 operator|.
-name|V_2_0_0
+name|V_2_0_0_beta1
 argument_list|)
 condition|)
 block|{
@@ -663,7 +663,7 @@ name|before
 argument_list|(
 name|Version
 operator|.
-name|V_2_0_0
+name|V_2_0_0_beta1
 argument_list|)
 condition|)
 block|{
