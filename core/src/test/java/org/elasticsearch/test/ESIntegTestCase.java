ea3e1075335d35a2a01c8dd3dccb56f297c182ee
Igor Motov <igor@motovs.org>
2015-09-15 19:34:48 -0400

    Tests: Make sure that the cluster state came from the same master before checking for discrepancies
    If nodes don't agree on the master, the cluster state doesn't have to match even if they have the same version
    
    
    Former-commit-id: d6f9fe96af8ca2b90379373adced4e375d15c7cd

--- core/src/test/java/org/elasticsearch/test/ESIntegTestCase.java
+++ core/src/test/java/org/elasticsearch/test/ESIntegTestCase.java
@@ -6963,7 +6963,8 @@ operator|.
 name|length
 argument_list|()
 decl_stmt|;
-comment|// Check that the non-master node has the same version of the cluster state as the master and that this node didn't disconnect from the master
+comment|// Check that the non-master node has the same version of the cluster state as the master and
+comment|// that the master node matches the master (otherwise there is no requirement for the cluster state to match)
 if|if
 condition|(
 name|masterClusterState
@@ -6976,17 +6977,17 @@ operator|.
 name|version
 argument_list|()
 operator|&&
+name|masterId
+operator|.
+name|equals
+argument_list|(
 name|localClusterState
 operator|.
 name|nodes
 argument_list|()
 operator|.
-name|nodes
+name|masterNodeId
 argument_list|()
-operator|.
-name|containsKey
-argument_list|(
-name|masterId
 argument_list|)
 condition|)
 block|{
