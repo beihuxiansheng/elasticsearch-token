23872095702922cddd4fa797a0c69e1bcc80d0f1
Jason Tedor <jason@tedor.me>
2015-09-15 10:46:59 -0400

    Remove unused import in o.e.a.t.TermVectorsFilter
    Former-commit-id: 2357e65fcc7d9856cc3e59e77b7846cea4068e6b

--- core/src/main/java/org/elasticsearch/action/termvectors/TermVectorsFilter.java
+++ core/src/main/java/org/elasticsearch/action/termvectors/TermVectorsFilter.java
@@ -18,22 +18,6 @@ end_package
 
 begin_import
 import|import
-name|com
-operator|.
-name|google
-operator|.
-name|common
-operator|.
-name|util
-operator|.
-name|concurrent
-operator|.
-name|AtomicLongMap
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
