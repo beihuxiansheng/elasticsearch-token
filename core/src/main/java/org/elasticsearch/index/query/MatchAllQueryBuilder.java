91bca1f2ec38f3fc9e055b1cc2a22d5aa3c0ff77
Christoph Büscher <christoph@elastic.co>
2015-09-16 17:40:02 +0200

    Query Refactoring: moving validation to constructors and setters
    This PR is an initial step to move the query validation we started
    to collect in the validate() method to the corresponding setters
    and constructors.
    
    
    Former-commit-id: 887399eebfb053cc0741ea82bbd7ddc3403a6c71

--- core/src/main/java/org/elasticsearch/index/query/MatchAllQueryBuilder.java
+++ core/src/main/java/org/elasticsearch/index/query/MatchAllQueryBuilder.java
@@ -192,19 +192,6 @@ return|;
 block|}
 annotation|@
 name|Override
-DECL|method|validate
-specifier|public
-name|QueryValidationException
-name|validate
-parameter_list|()
-block|{
-comment|// nothing to validate
-return|return
-literal|null
-return|;
-block|}
-annotation|@
-name|Override
 DECL|method|doEquals
 specifier|protected
 name|boolean
