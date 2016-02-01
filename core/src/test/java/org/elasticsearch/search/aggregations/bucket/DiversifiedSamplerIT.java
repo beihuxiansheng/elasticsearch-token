3b3af5ddb89c12f7b56dd2f4fe5c8acbd1ca66e8
Colin Goodheart-Smithe <colings86@users.noreply.github.com>
2016-02-01 13:57:38 +0000

    fixed issue from merge
    Former-commit-id: 2d2e9e06218153e725310e1b24cefb870cda12cc

--- core/src/test/java/org/elasticsearch/search/aggregations/bucket/DiversifiedSamplerIT.java
+++ core/src/test/java/org/elasticsearch/search/aggregations/bucket/DiversifiedSamplerIT.java
@@ -1754,7 +1754,7 @@ argument_list|()
 argument_list|,
 name|greaterThan
 argument_list|(
-literal|0l
+literal|0L
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -1928,7 +1928,7 @@ argument_list|()
 argument_list|,
 name|equalTo
 argument_list|(
-literal|0l
+literal|0L
 argument_list|)
 argument_list|)
 expr_stmt|;
