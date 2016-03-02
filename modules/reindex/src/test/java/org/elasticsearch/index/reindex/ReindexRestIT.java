b1791412fd9bed5a01ed4f37e1193606c841305e
Nik Everett <nik9000@gmail.com>
2016-03-02 08:29:32 -0500

    Silence reindex's rest tests
    They are failing sporadically in CI.
    
    
    Former-commit-id: aa0ef84f5a6dc1a4621e5b896f2177bb904b0dce

--- modules/reindex/src/test/java/org/elasticsearch/index/reindex/ReindexRestIT.java
+++ modules/reindex/src/test/java/org/elasticsearch/index/reindex/ReindexRestIT.java
@@ -48,6 +48,22 @@ begin_import
 import|import
 name|org
 operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|util
+operator|.
+name|LuceneTestCase
+operator|.
+name|AwaitsFix
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
 name|elasticsearch
 operator|.
 name|test
@@ -99,6 +115,13 @@ import|;
 end_import
 
 begin_class
+annotation|@
+name|AwaitsFix
+argument_list|(
+name|bugUrl
+operator|=
+literal|"https://github.com/elastic/elasticsearch/issues/16906"
+argument_list|)
 DECL|class|ReindexRestIT
 specifier|public
 class|class
