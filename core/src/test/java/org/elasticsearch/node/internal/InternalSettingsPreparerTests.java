d7c74839dd6adc128a49b97084c695f2691b1e3e
Jason Tedor <jason@tedor.me>
2016-03-22 09:48:06 -0400

    Use mock filesystem during install plugins tests
    This commit sets up the default filesystem used during install plugins
    tests. A hack is neeeded to handle the temporary directory because the
    system property "java.io.tmpdir" will have been initialized to a value
    that is sensible for the default filesystem, but not necessarily to a
    value that makes sense for the mock filesystem in use during the
    tests. This property is restored after each test.
    
    
    Former-commit-id: 5dc48e71d04ff749a697df96023ebf9352ce91c6

--- core/src/test/java/org/elasticsearch/node/internal/InternalSettingsPreparerTests.java
+++ core/src/test/java/org/elasticsearch/node/internal/InternalSettingsPreparerTests.java
@@ -40,16 +40,6 @@ begin_import
 import|import
 name|java
 operator|.
-name|net
-operator|.
-name|URI
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
 name|nio
 operator|.
 name|file
