370b25334a0e89265342d7955de2d2d632367a34
Martijn van Groningen <martijn.v.groningen@gmail.com>
2016-04-06 13:34:45 +0200

    test: don't do fuzzy on bool field
    Former-commit-id: 78d2311c5fd502088bd00625956326834538c785

--- core/src/test/java/org/elasticsearch/index/query/AbstractQueryTestCase.java
+++ core/src/test/java/org/elasticsearch/index/query/AbstractQueryTestCase.java
@@ -5732,6 +5732,12 @@ name|randomTimeValue
 argument_list|()
 argument_list|)
 return|;
+case|case
+name|BOOLEAN_FIELD_NAME
+case|:
+return|return
+literal|null
+return|;
 default|default:
 if|if
 condition|(
