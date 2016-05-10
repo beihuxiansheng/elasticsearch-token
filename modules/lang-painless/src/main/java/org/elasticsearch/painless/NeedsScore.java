fb42ec06965cc2b7d949820893f7f86edf562474
Robert Muir <rmuir@apache.org>
2016-05-10 11:23:24 -0400

    hack
    Former-commit-id: 546aed8390952c2802a3fb13026f42f6fa9c5e6b

---
+++ modules/lang-painless/src/main/java/org/elasticsearch/painless/NeedsScore.java
@@ -0,0 +1,26 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_package
+DECL|package|org.elasticsearch.painless
+package|package
+name|org
+operator|.
+name|elasticsearch
+operator|.
+name|painless
+package|;
+end_package
+
+begin_comment
+comment|/** Marker interface that a generated {@link Executable} uses the {@code _score} value */
+end_comment
+
+begin_interface
+DECL|interface|NeedsScore
+specifier|public
+interface|interface
+name|NeedsScore
+block|{ }
+end_interface
+
+end_unit
+
