a3d593a31c3b9db1e2964e5e634456f5534b6e32
Robert Muir <rmuir@apache.org>
2016-06-06 17:58:00 -0400

    ban slow methods in MethodHandle
    Former-commit-id: 23d7cdd043a14367f88f8f74a81d6789bf7bbf40

--- modules/lang-painless/src/main/java/org/elasticsearch/painless/DefBootstrap.java
+++ modules/lang-painless/src/main/java/org/elasticsearch/painless/DefBootstrap.java
@@ -10,6 +10,18 @@ name|painless
 package|;
 end_package
 
+begin_import
+import|import
+name|org
+operator|.
+name|elasticsearch
+operator|.
+name|common
+operator|.
+name|SuppressForbidden
+import|;
+end_import
+
 begin_comment
 comment|/*  * Licensed to Elasticsearch under one or more contributor  * license agreements. See the NOTICE file distributed with  * this work for additional information regarding copyright  * ownership. Elasticsearch licenses this file to you under  * the Apache License, Version 2.0 (the "License"); you may  * not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  *    http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied.  See the License for the  * specific language governing permissions and limitations  * under the License.  */
 end_comment
@@ -432,6 +444,13 @@ throw|;
 block|}
 block|}
 comment|/**          * Called when a new type is encountered (or, when we have encountered more than {@code MAX_DEPTH}          * types at this call site and given up on caching).          */
+annotation|@
+name|SuppressForbidden
+argument_list|(
+name|reason
+operator|=
+literal|"slow path"
+argument_list|)
 DECL|method|fallback
 name|Object
 name|fallback
