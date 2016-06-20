da1f7a55538a03881add17fb935091273c0782ed
Robert Muir <rmuir@apache.org>
2016-06-20 08:32:53 -0400

    remove unused import
    Former-commit-id: 28b1b149abe54909336c41b22bc7a208c54bd763

--- modules/lang-painless/src/main/java/org/elasticsearch/painless/DefMath.java
+++ modules/lang-painless/src/main/java/org/elasticsearch/painless/DefMath.java
@@ -90,16 +90,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|IdentityHashMap
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|Map
 import|;
 end_import
