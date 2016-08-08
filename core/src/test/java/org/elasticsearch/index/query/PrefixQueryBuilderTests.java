5ecacd9fe78b4c405c024eeb7dff34c2a5d95735
javanna <cavannaluca@gmail.com>
2016-08-08 19:42:26 +0200

    Throw parsing error if prefix query contains multiple fields in its short version
    Former-commit-id: 8f485b361416fff6653fdd55f4c255ac9843d0c5

--- core/src/test/java/org/elasticsearch/index/query/PrefixQueryBuilderTests.java
+++ core/src/test/java/org/elasticsearch/index/query/PrefixQueryBuilderTests.java
@@ -788,6 +788,47 @@ name|getMessage
 argument_list|()
 argument_list|)
 expr_stmt|;
+name|String
+name|shortJson
+init|=
+literal|"{\n"
+operator|+
+literal|"    \"prefix\": {\n"
+operator|+
+literal|"      \"user1\": \"ki\",\n"
+operator|+
+literal|"      \"user2\": \"ki\"\n"
+operator|+
+literal|"    }\n"
+operator|+
+literal|"}"
+decl_stmt|;
+name|e
+operator|=
+name|expectThrows
+argument_list|(
+name|ParsingException
+operator|.
+name|class
+argument_list|,
+parameter_list|()
+lambda|->
+name|parseQuery
+argument_list|(
+name|shortJson
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|"[prefix] query doesn't support multiple fields, found [user1] and [user2]"
+argument_list|,
+name|e
+operator|.
+name|getMessage
+argument_list|()
+argument_list|)
+expr_stmt|;
 block|}
 block|}
 end_class
