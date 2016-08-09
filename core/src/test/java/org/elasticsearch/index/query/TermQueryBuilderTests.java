0b51bb1a7112c6f627686919bd357731980d9002
javanna <cavannaluca@gmail.com>
2016-08-09 09:50:12 +0200

    [TEST] test that term query short syntax throws error when multiple fields are provided
    Former-commit-id: bbf40ca0cf56b8475d526dac44e729e9eaf38a38

--- core/src/test/java/org/elasticsearch/index/query/TermQueryBuilderTests.java
+++ core/src/test/java/org/elasticsearch/index/query/TermQueryBuilderTests.java
@@ -806,6 +806,47 @@ name|getMessage
 argument_list|()
 argument_list|)
 expr_stmt|;
+name|String
+name|shortJson
+init|=
+literal|"{\n"
+operator|+
+literal|"  \"term\" : {\n"
+operator|+
+literal|"    \"message1\" : \"this\",\n"
+operator|+
+literal|"    \"message2\" : \"this\"\n"
+operator|+
+literal|"  }\n"
+operator|+
+literal|"}"
+decl_stmt|;
+name|e
+operator|=
+name|expectThrows
+argument_list|(
+name|ParsingException
+operator|.
+name|class
+argument_list|,
+parameter_list|()
+lambda|->
+name|parseQuery
+argument_list|(
+name|shortJson
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|"[term] query does not support different field names, use [bool] query instead"
+argument_list|,
+name|e
+operator|.
+name|getMessage
+argument_list|()
+argument_list|)
+expr_stmt|;
 block|}
 block|}
 end_class
