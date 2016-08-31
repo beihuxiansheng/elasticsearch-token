2e0f3f3f5d0895dad02eca2590f2ac417f02099b
Nik Everett <nik9000@gmail.com>
2016-08-31 14:45:03 -0400

    Revert "Don't allow XContentBuilder#writeValue(TimeValue)"
    This reverts commit 03a85333e76ce2711a69c8fcce1016387219e5d0 [formerly 7f70c00dad0574f35adea06c33ee2939ecd3e477].
    
    
    Former-commit-id: bd93c7054c4c9ef3309e55c2e6eabbff983e469c

--- core/src/main/java/org/elasticsearch/common/xcontent/XContentBuilder.java
+++ core/src/main/java/org/elasticsearch/common/xcontent/XContentBuilder.java
@@ -4709,15 +4709,6 @@ argument_list|()
 expr_stmt|;
 return|return;
 block|}
-assert|assert
-literal|false
-operator|==
-name|value
-operator|instanceof
-name|TimeValue
-operator|:
-literal|"Use timeValueField instead"
-assert|;
 name|Class
 argument_list|<
 name|?
