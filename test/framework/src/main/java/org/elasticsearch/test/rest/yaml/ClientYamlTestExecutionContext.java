3c183595655eafe103eb266c0e684c138d9bdfc2
Simon Willnauer <simonw@apache.org>
2016-09-02 10:07:58 +0200

    [TEST] fix possible NPE in ClientYamlTestExecutionContext
    Former-commit-id: 825b80f2a66d66b2598a8246d5c8cd0d7754fb82

--- test/framework/src/main/java/org/elasticsearch/test/rest/yaml/ClientYamlTestExecutionContext.java
+++ test/framework/src/main/java/org/elasticsearch/test/rest/yaml/ClientYamlTestExecutionContext.java
@@ -371,6 +371,21 @@ throw|;
 block|}
 finally|finally
 block|{
+comment|// if we hit a bad exception the response is null
+name|Object
+name|repsponseBody
+init|=
+name|response
+operator|!=
+literal|null
+condition|?
+name|response
+operator|.
+name|getBody
+argument_list|()
+else|:
+literal|null
+decl_stmt|;
 comment|//we always stash the last response body
 name|stash
 operator|.
@@ -378,10 +393,7 @@ name|stashValue
 argument_list|(
 literal|"body"
 argument_list|,
-name|response
-operator|.
-name|getBody
-argument_list|()
+name|repsponseBody
 argument_list|)
 expr_stmt|;
 block|}
