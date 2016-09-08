acb67568ad129a7364e3820cbccaff8e9fa8d245
Simon Willnauer <simonw@apache.org>
2016-09-08 12:55:30 +0200

    Prepare master branch to be 6.0.0-alpha1
    Former-commit-id: f31954581477710616791e77d802e43897c3a0b0

--- core/src/test/java/org/elasticsearch/bwcompat/OldIndexBackwardsCompatibilityIT.java
+++ core/src/test/java/org/elasticsearch/bwcompat/OldIndexBackwardsCompatibilityIT.java
@@ -1544,6 +1544,19 @@ if|if
 condition|(
 name|v
 operator|.
+name|onOrBefore
+argument_list|(
+name|Version
+operator|.
+name|V_5_0_0
+argument_list|)
+condition|)
+continue|continue;
+comment|// we have not yet released this
+if|if
+condition|(
+name|v
+operator|.
 name|isAlpha
 argument_list|()
 condition|)
