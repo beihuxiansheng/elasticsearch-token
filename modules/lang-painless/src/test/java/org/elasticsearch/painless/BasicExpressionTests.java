c78860c3cb706f38ce012d8fa8c11f9994c5a0da
Nik Everett <nik9000@gmail.com>
2016-10-30 12:25:51 -0400

    Implement reading from null safe dereferences
    Null safe dereferences make handling null or missing values shorter.
    Compare without:
    ```
    if (ctx._source.missing != null && ctx._source.missing.foo != null) {
      ctx._source.foo_length = ctx.source.missing.foo.length()
    }
    ```
    
    To with:
    ```
    Integer length = ctx._source.missing?.foo?.length();
    if (length != null) {
      ctx._source.foo_length = length
    }
    ```
    
    Combining this with the as of yet unimplemented elvis operator allows
    for very concise defaults for nulls:
    ```
    ctx._source.foo_length = ctx._source.missing?.foo?.length() ?: 0;
    ```
    
    Since you have to start somewhere, we started with null safe dereferenes.
    
    Anyway, this is a feature borrowed from groovy. Groovy allows writing to
    null values like:
    ```
    def v = null
    v?.field = 'cat'
    ```
    And the writes are simply ignored. Painless doesn't support this at this
    point because it'd be complex to implement and maybe not all that useful.
    
    There is no runtime cost for this feature if it is not used. When it is
    used we implement it fairly efficiently, adding a jump rather than a
    temporary variable.
    
    This should also work fairly well with doc values.
    
    
    Former-commit-id: d03b8e4abb2d42f5020cb1ba985f4d287d8ab81d

--- modules/lang-painless/src/test/java/org/elasticsearch/painless/BasicExpressionTests.java
+++ modules/lang-painless/src/test/java/org/elasticsearch/painless/BasicExpressionTests.java
@@ -20,6 +20,18 @@ name|Collections
 import|;
 end_import
 
+begin_import
+import|import static
+name|java
+operator|.
+name|util
+operator|.
+name|Collections
+operator|.
+name|singletonMap
+import|;
+end_import
+
 begin_comment
 comment|/*  * Licensed to Elasticsearch under one or more contributor  * license agreements. See the NOTICE file distributed with  * this work for additional information regarding copyright  * ownership. Elasticsearch licenses this file to you under  * the Apache License, Version 2.0 (the "License"); you may  * not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  *    http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied.  See the License for the  * specific language governing permissions and limitations  * under the License.  */
 end_comment
@@ -744,6 +756,522 @@ literal|"boolean t = true, f = false; return t&& (f || t);"
 argument_list|)
 argument_list|)
 expr_stmt|;
+block|}
+DECL|method|testNullSafeDeref
+specifier|public
+name|void
+name|testNullSafeDeref
+parameter_list|()
+block|{
+comment|// Objects in general
+name|assertNull
+argument_list|(
+name|exec
+argument_list|(
+literal|"String a = null; return a?.toString()"
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Call
+name|assertNull
+argument_list|(
+name|exec
+argument_list|(
+literal|"String a = null; return a?.length()"
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Call and box
+name|assertEquals
+argument_list|(
+literal|"foo"
+argument_list|,
+name|exec
+argument_list|(
+literal|"String a = 'foo'; return a?.toString()"
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Call
+name|assertEquals
+argument_list|(
+name|Integer
+operator|.
+name|valueOf
+argument_list|(
+literal|3
+argument_list|)
+argument_list|,
+name|exec
+argument_list|(
+literal|"String a = 'foo'; return a?.length()"
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Call and box
+comment|// Maps
+name|assertNull
+argument_list|(
+name|exec
+argument_list|(
+literal|"Map a = null;        return a?.toString()"
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Call
+name|assertNull
+argument_list|(
+name|exec
+argument_list|(
+literal|"Map a = null;        return a?.size()"
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Call and box
+name|assertNull
+argument_list|(
+name|exec
+argument_list|(
+literal|"Map a = null;        return a?.other"
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Read shortcut
+name|assertEquals
+argument_list|(
+literal|"{}"
+argument_list|,
+name|exec
+argument_list|(
+literal|"Map a = [:];         return a?.toString()"
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Call
+name|assertEquals
+argument_list|(
+literal|0
+argument_list|,
+name|exec
+argument_list|(
+literal|"Map a = [:];         return a?.size()"
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Call and box
+name|assertEquals
+argument_list|(
+literal|1
+argument_list|,
+name|exec
+argument_list|(
+literal|"Map a = ['other':1]; return a?.other"
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Read shortcut
+comment|// Array
+comment|// Since you can't invoke methods on arrays we skip the toString and hashCode tests
+name|assertNull
+argument_list|(
+name|exec
+argument_list|(
+literal|"int[] a = null;             return a?.length"
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Length (boxed)
+name|assertEquals
+argument_list|(
+literal|2
+argument_list|,
+name|exec
+argument_list|(
+literal|"int[] a = new int[] {2, 3}; return a?.length"
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Length (boxed)
+comment|// Def
+name|assertNull
+argument_list|(
+name|exec
+argument_list|(
+literal|"def a = null;                                         return a?.getX()"
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Invoke
+name|assertNull
+argument_list|(
+name|exec
+argument_list|(
+literal|"def a = null;                                         return a?.x"
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Read shortcut
+name|assertEquals
+argument_list|(
+literal|0
+argument_list|,
+name|exec
+argument_list|(
+literal|"def a = new org.elasticsearch.painless.FeatureTest(); return a?.getX()"
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Invoke
+name|assertEquals
+argument_list|(
+literal|0
+argument_list|,
+name|exec
+argument_list|(
+literal|"def a = new org.elasticsearch.painless.FeatureTest(); return a?.x"
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Read shortcut
+comment|// Results from maps (should just work but let's test anyway)
+name|FeatureTest
+name|t
+init|=
+operator|new
+name|FeatureTest
+argument_list|()
+decl_stmt|;
+name|assertNull
+argument_list|(
+name|exec
+argument_list|(
+literal|"Map a = ['thing': params.t]; return a.other?.getX()"
+argument_list|,
+name|singletonMap
+argument_list|(
+literal|"t"
+argument_list|,
+name|t
+argument_list|)
+argument_list|,
+literal|true
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertNull
+argument_list|(
+name|exec
+argument_list|(
+literal|"Map a = ['thing': params.t]; return a.other?.x"
+argument_list|,
+name|singletonMap
+argument_list|(
+literal|"t"
+argument_list|,
+name|t
+argument_list|)
+argument_list|,
+literal|true
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertNull
+argument_list|(
+name|exec
+argument_list|(
+literal|"def a = ['thing': params.t]; return a.other?.getX()"
+argument_list|,
+name|singletonMap
+argument_list|(
+literal|"t"
+argument_list|,
+name|t
+argument_list|)
+argument_list|,
+literal|true
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertNull
+argument_list|(
+name|exec
+argument_list|(
+literal|"def a = ['thing': params.t]; return a.other?.x"
+argument_list|,
+name|singletonMap
+argument_list|(
+literal|"t"
+argument_list|,
+name|t
+argument_list|)
+argument_list|,
+literal|true
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|0
+argument_list|,
+name|exec
+argument_list|(
+literal|"Map a = ['other': params.t]; return a.other?.getX()"
+argument_list|,
+name|singletonMap
+argument_list|(
+literal|"t"
+argument_list|,
+name|t
+argument_list|)
+argument_list|,
+literal|true
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|0
+argument_list|,
+name|exec
+argument_list|(
+literal|"Map a = ['other': params.t]; return a.other?.x"
+argument_list|,
+name|singletonMap
+argument_list|(
+literal|"t"
+argument_list|,
+name|t
+argument_list|)
+argument_list|,
+literal|true
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|0
+argument_list|,
+name|exec
+argument_list|(
+literal|"def a = ['other': params.t]; return a.other?.getX()"
+argument_list|,
+name|singletonMap
+argument_list|(
+literal|"t"
+argument_list|,
+name|t
+argument_list|)
+argument_list|,
+literal|true
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|0
+argument_list|,
+name|exec
+argument_list|(
+literal|"def a = ['other': params.t]; return a.other?.x"
+argument_list|,
+name|singletonMap
+argument_list|(
+literal|"t"
+argument_list|,
+name|t
+argument_list|)
+argument_list|,
+literal|true
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Chains
+name|assertNull
+argument_list|(
+name|exec
+argument_list|(
+literal|"Map a = ['thing': ['cat': params.t]]; return a.other?.cat?.getX()"
+argument_list|,
+name|singletonMap
+argument_list|(
+literal|"t"
+argument_list|,
+name|t
+argument_list|)
+argument_list|,
+literal|true
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertNull
+argument_list|(
+name|exec
+argument_list|(
+literal|"Map a = ['thing': ['cat': params.t]]; return a.other?.cat?.x"
+argument_list|,
+name|singletonMap
+argument_list|(
+literal|"t"
+argument_list|,
+name|t
+argument_list|)
+argument_list|,
+literal|true
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertNull
+argument_list|(
+name|exec
+argument_list|(
+literal|"def a = ['thing': ['cat': params.t]]; return a.other?.cat?.getX()"
+argument_list|,
+name|singletonMap
+argument_list|(
+literal|"t"
+argument_list|,
+name|t
+argument_list|)
+argument_list|,
+literal|true
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertNull
+argument_list|(
+name|exec
+argument_list|(
+literal|"def a = ['thing': ['cat': params.t]]; return a.other?.cat?.x"
+argument_list|,
+name|singletonMap
+argument_list|(
+literal|"t"
+argument_list|,
+name|t
+argument_list|)
+argument_list|,
+literal|true
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|0
+argument_list|,
+name|exec
+argument_list|(
+literal|"Map a = ['other': ['cat': params.t]]; return a.other?.cat?.getX()"
+argument_list|,
+name|singletonMap
+argument_list|(
+literal|"t"
+argument_list|,
+name|t
+argument_list|)
+argument_list|,
+literal|true
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|0
+argument_list|,
+name|exec
+argument_list|(
+literal|"Map a = ['other': ['cat': params.t]]; return a.other?.cat?.x"
+argument_list|,
+name|singletonMap
+argument_list|(
+literal|"t"
+argument_list|,
+name|t
+argument_list|)
+argument_list|,
+literal|true
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|0
+argument_list|,
+name|exec
+argument_list|(
+literal|"def a = ['other': ['cat': params.t]]; return a.other?.cat?.getX()"
+argument_list|,
+name|singletonMap
+argument_list|(
+literal|"t"
+argument_list|,
+name|t
+argument_list|)
+argument_list|,
+literal|true
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|0
+argument_list|,
+name|exec
+argument_list|(
+literal|"def a = ['other': ['cat': params.t]]; return a.other?.cat?.x"
+argument_list|,
+name|singletonMap
+argument_list|(
+literal|"t"
+argument_list|,
+name|t
+argument_list|)
+argument_list|,
+literal|true
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Check that we don't try to cast when the LHS doesn't provide an "expected" value
+name|assertNull
+argument_list|(
+name|exec
+argument_list|(
+literal|"def a = [:];\n"
+operator|+
+literal|"a.missing_length = a.missing?.length();\n"
+operator|+
+literal|"return a.missing_length"
+argument_list|,
+literal|true
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|3
+argument_list|,
+name|exec
+argument_list|(
+literal|"def a = [:];\n"
+operator|+
+literal|"a.missing = 'foo';\n"
+operator|+
+literal|"a.missing_length = a.missing?.length();\n"
+operator|+
+literal|"return a.missing_length"
+argument_list|,
+literal|true
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|// Writes, all unsupported at this point
+comment|//        assertEquals(null, exec("org.elasticsearch.painless.FeatureTest a = null; return a?.x"));            // Read field
+comment|//        assertEquals(null, exec("org.elasticsearch.painless.FeatureTest a = null; a?.x = 7; return a?.x"));  // Write field
+comment|//        assertEquals(null, exec("Map a = null; a?.other = 'wow'; return a?.other")); // Write shortcut
+comment|//        assertEquals(null, exec("def a = null; a?.other = 'cat'; return a?.other")); // Write shortcut
+comment|//        assertEquals(null, exec("Map a = ['thing': 'bar']; a.other?.cat = 'no'; return a.other?.cat"));
+comment|//        assertEquals(null, exec("def a = ['thing': 'bar']; a.other?.cat = 'no'; return a.other?.cat"));
+comment|//        assertEquals(null, exec("Map a = ['thing': 'bar']; a.other?.cat?.dog = 'wombat'; return a.other?.cat?.dog"));
+comment|//        assertEquals(null, exec("def a = ['thing': 'bar']; a.other?.cat?.dog = 'wombat'; return a.other?.cat?.dog"));
 block|}
 block|}
 end_class
