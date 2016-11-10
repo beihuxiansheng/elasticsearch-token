b07f3c54f96f6188a4b950a3a9aba197995be340
Jack Conradson <osjdconrad@gmail.com>
2016-11-10 11:46:14 -0800

    Remove accidental import.
    Former-commit-id: 834976823ae686b878f9b190d6981f77310c4f31

--- core/src/test/java/org/elasticsearch/script/ScriptServiceTests.java
+++ core/src/test/java/org/elasticsearch/script/ScriptServiceTests.java
@@ -16,20 +16,6 @@ end_package
 
 begin_import
 import|import
-name|com
-operator|.
-name|sun
-operator|.
-name|javafx
-operator|.
-name|collections
-operator|.
-name|ElementObservableListDecorator
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|elasticsearch
