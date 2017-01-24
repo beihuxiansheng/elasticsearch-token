a55d1273b1bc97ef8fe09d3242f99539b58f5030
Jim Ferenczi <jim.ferenczi@elastic.co>
2017-01-24 08:33:59 +0100

    Add BWC tests for field collapsing
    Field collapsing is supported from version 5.3
    
    
    Former-commit-id: 868b12b5484ff32aa93765e2a8bd14e29d2ce750

--- core/src/main/java/org/elasticsearch/search/builder/SearchSourceBuilder.java
+++ core/src/main/java/org/elasticsearch/search/builder/SearchSourceBuilder.java
@@ -1593,7 +1593,7 @@ name|onOrAfter
 argument_list|(
 name|Version
 operator|.
-name|V_6_0_0_alpha1_UNRELEASED
+name|V_5_3_0_UNRELEASED
 argument_list|)
 condition|)
 block|{
@@ -1911,7 +1911,7 @@ name|onOrAfter
 argument_list|(
 name|Version
 operator|.
-name|V_6_0_0_alpha1_UNRELEASED
+name|V_5_3_0_UNRELEASED
 argument_list|)
 condition|)
 block|{
