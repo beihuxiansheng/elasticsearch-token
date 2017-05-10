f1a74c4b2c4316d834739e05918b458f3e18b086
Tanguy Leroux <tlrx.dev@gmail.com>
2017-05-10 12:31:28 +0200

    [Test] Fix wrong import
    Former-commit-id: 3e4406f044676d50aab0e7135d2a289a929ea84b

--- core/src/test/java/org/elasticsearch/search/aggregations/bucket/range/InternalRangeTestCase.java
+++ core/src/test/java/org/elasticsearch/search/aggregations/bucket/range/InternalRangeTestCase.java
@@ -44,7 +44,9 @@ name|search
 operator|.
 name|aggregations
 operator|.
-name|InternalAggregationTestCase
+name|pipeline
+operator|.
+name|PipelineAggregator
 import|;
 end_import
 
@@ -54,13 +56,9 @@ name|org
 operator|.
 name|elasticsearch
 operator|.
-name|search
+name|test
 operator|.
-name|aggregations
-operator|.
-name|pipeline
-operator|.
-name|PipelineAggregator
+name|InternalAggregationTestCase
 import|;
 end_import
 
