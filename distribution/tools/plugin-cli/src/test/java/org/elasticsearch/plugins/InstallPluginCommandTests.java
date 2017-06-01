cdba44106ef9bb3abed4191b9bfc370f6630e6a8
Jason Tedor <jason@tedor.me>
2017-06-01 08:56:25 -0400

    Remove import from InstallPluginCommandTests
    This commit removes an unused import from
    InstallPluginCommandTests.java.
    
    
    Former-commit-id: 5961e1d6abdb0bc3edca2af0348d9c3a8d440843

--- distribution/tools/plugin-cli/src/test/java/org/elasticsearch/plugins/InstallPluginCommandTests.java
+++ distribution/tools/plugin-cli/src/test/java/org/elasticsearch/plugins/InstallPluginCommandTests.java
@@ -250,16 +250,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|AfterClass
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|Before
 import|;
 end_import
